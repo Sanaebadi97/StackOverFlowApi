b32da88376b0e92ea6012f2267a84c46662e8bb2
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-19 12:40:35 +0430

    created data and domain module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
+    val kotlin_version by extra("1.4.10")
     repositories {
         google()
         jcenter()
@@ -10,6 +11,7 @@ buildscript {
         classpath (BuildPlugins.androidGradlePlugin)
         classpath (BuildPlugins.kotlinGradlePlugin)
         classpath (BuildPlugins.safeArgsPlugin)
+        "classpath"("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
---
+++ domain/src/main/java/info/sanaebadi/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package info.sanaebadi.domain
+
+class MyClass {
+}
