d91f9d084db5c0aab638b806b76f8960a152c3cf
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-23 23:25:52 +0330

    created user list with bug ! I will fix it early

--- app/src/main/java/info/sanaebadi/stackoverflowproject/mvvm/feature/view/viewModel/UserViewModel.kt
+++ app/src/main/java/info/sanaebadi/stackoverflowproject/mvvm/feature/view/viewModel/UserViewModel.kt
@@ -4,28 +4,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import info.sanaebadi.domain.interactor.base.BaseSingleObserver
 import info.sanaebadi.domain.interactor.user.UserUseCase
+import info.sanaebadi.domain.model.user.User
+import info.sanaebadi.stackoverflowproject.mapper.UserPresentationMapper
 import info.sanaebadi.stackoverflowproject.model.user.UserPre
 import info.sanaebadi.stackoverflowproject.model.user.UserListModelPre
 import info.sanaebadi.stackoverflowproject.mvvm.feature.view.viewModel.base.MutableViewModel
 import javax.inject.Inject
 
 class UserViewModel @Inject constructor(
-    private val userUseCase: UserUseCase
+    private val userUseCase: UserUseCase ,
+    private val userPresentationMapper: UserPresentationMapper
 ) : ViewModel() {
 
-    var userList = MutableLiveData<MutableViewModel<UserListModelPre>>()
+    var userList = MutableLiveData<MutableViewModel<User>>()
 
     fun getUserList(page: Int) {
-        val value = MutableViewModel<UserListModelPre>()
+        val value = MutableViewModel<User>()
         value.setLoading(true)
         userList.postValue(value)
 
-        userUseCase.execute(object : BaseSingleObserver<UserPre>() {
-            override fun onSuccess(t: UserPre) {
+        userUseCase.execute(object : BaseSingleObserver<User>() {
+            override fun onSuccess(t: User) {
                 super.onSuccess(t)
                 value.setLoading(false)
                 value.setThrowable(null)
-                value.setData()
+                value.setData(userPresentationMapper.mtoPresentation(t.displayName))
                 userList.postValue(value)
 
             }
