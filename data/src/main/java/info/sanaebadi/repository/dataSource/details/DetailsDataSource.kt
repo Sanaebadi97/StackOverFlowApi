7fd75730bb1249f3c1d208176e669f333ad5a0ed
Sana Ebadi <sanaebadi97@gmail.com>
2020-09-28 18:05:48 +0330

    added Online Data source

--- data/src/main/java/info/sanaebadi/repository/dataSource/details/DetailsDataSource.kt
+++ data/src/main/java/info/sanaebadi/repository/dataSource/details/DetailsDataSource.kt
@@ -1,13 +1,13 @@
 package info.sanaebadi.repository.dataSource.details
 
-import info.sanaebadi.entity.user.AnswerEntity
-import info.sanaebadi.entity.user.QuestionEntity
+import info.sanaebadi.entity.user.AnswerListEntity
+import info.sanaebadi.entity.user.QuestionListEntity
 import io.reactivex.Single
 
 
 interface DetailsDataSource {
-    fun getQuestionsByUser(userId: Long): Single<List<QuestionEntity>>
-    fun getAnswersByUser(userId: Long): Single<List<AnswerEntity>>
-    fun getFavoritesByUser(userId: Long): Single<List<QuestionEntity>>
-    fun getQuestionsById(ids: List<Long>, userId: Long): Single<List<QuestionEntity>>
+    fun getQuestionsByUser(userId: Long): Single<QuestionListEntity>
+    fun getAnswersByUser(userId: Long): Single<AnswerListEntity>
+    fun getFavoritesByUser(userId: Long): Single<QuestionListEntity>
+    fun getQuestionsById(ids: List<Long>, userId: Long): Single<QuestionListEntity>
 }
