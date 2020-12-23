2f25b857105917a4cef92a9dc5d8d401b30a2049
Sana Ebadi <26750131+Sanaebadi97@users.noreply.github.com>
2020-12-23 16:04:46 +0330

    Update Owner.kt

--- domain/src/main/java/info/sanaebadi/domain/model/user/Owner.kt
+++ domain/src/main/java/info/sanaebadi/domain/model/user/Owner.kt
@@ -3,6 +3,7 @@ package info.sanaebadi.domain.model.user
 import info.sanaebadi.domain.model.base.BaseDomainModel
 
 
+
 data class Owner(var userId: Long) : BaseDomainModel {
 
     constructor() : this(-1)
